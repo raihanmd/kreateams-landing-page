6d53b063670e74b17046c66746b8d3d8645415c2
raihanmd <mraihanf471@gmail.com>
2024-05-15 17:39:46 +0700

    fix: layout our program

--- src/app/_libs/components/Program/Card/index.tsx
+++ src/app/_libs/components/Program/Card/index.tsx
@@ -5,31 +5,34 @@ import { cn } from "@/lib/utils";
 
 export default function Card({ program, index }: ProgramCardProps) {
   return (
-    <div className="grid w-full place-items-center gap-5 py-10 xl:grid-cols-12">
-      <div className="col-span-3 flex flex-col font-bold">
-        <p className="text-3xl">{program.title}</p>
-        <p
-          className={cn(
-            "text-[5rem] text-transparent [-webkit-text-stroke:_2px_#f4b100] [text-stroke:_2px_#f4b100]",
-            {
-              "text-primary [-webkit-text-stroke:_0px] [text-stroke:_0px]":
-                index % 2 !== 0,
-            },
-          )}
-        >
-          0{index + 1}
-        </p>
+    <div className="grid w-full place-items-center gap-5 px-5 py-10 lg:grid-cols-12 lg:place-items-start 2xl:px-0">
+      <div className="grid gap-5 lg:col-span-6 xl:col-span-8 xl:grid-cols-5">
+        <div className="flex flex-col font-bold xl:col-span-2">
+          <p className="text-3xl">{program.title}</p>
+          <p
+            className={cn(
+              "text-[5rem] text-transparent [-webkit-text-stroke:_2px_#f4b100] [text-stroke:_2px_#f4b100]",
+              {
+                "text-primary [-webkit-text-stroke:_0px] [text-stroke:_0px]":
+                  index % 2 !== 0,
+              },
+            )}
+          >
+            0{index + 1}
+          </p>
+        </div>
+        <div className="flex items-start justify-start xl:col-span-3">
+          <p>{program.content}</p>
+        </div>
       </div>
-      <div className="col-span-5 flex items-start justify-start">
-        <p>{program.content}</p>
-      </div>
-      <div className="col-span-4">
+      <div className="place-self-center lg:col-span-6 xl:col-span-4">
         <Image
+          draggable={false}
           src={program.img}
           alt="Workshop"
-          width={400}
-          height={400}
-          className="h-[300px] w-full rounded-md object-cover"
+          width={450}
+          height={300}
+          className="h-[300px] w-[450px] rounded-md object-cover"
         />
       </div>
     </div>
--- src/app/_libs/components/Program/index.tsx
+++ src/app/_libs/components/Program/index.tsx
@@ -6,8 +6,8 @@ export default function Program() {
     <div className="mt-20 bg-slate-800 text-background" id="our-programs">
       <div className="container flex flex-col gap-5 py-20">
         <div className="flex flex-col gap-3 font-neue">
-          <p className="text-3xl">Our Programs -</p>
-          <h3 className="text-7xl">
+          <p className="text-xl md:text-3xl">Our Programs -</p>
+          <h3 className="text-5xl md:text-7xl">
             Ignite the Creative Journey: Dive into Our Vibrant Program Offerings
           </h3>
         </div>
